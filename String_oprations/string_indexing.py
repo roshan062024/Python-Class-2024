8798f1ededefafd436ca3d9160f684c7ea7234b4
roshan062024 <157616494+roshan062024@users.noreply.github.com>
2024-02-06 19:34:27 +0000

    String Oprations

---
+++ String_oprations/string_indexing.py
@@ -0,0 +1,16 @@
+"""
+String Operations - Indexing
+"""
+
+
+language = "Python Programming"
+print(language, type(language))
+print("language    :", language)
+print("language[4]:", language[4])
+print("language[16] :", language[16])
+print("language[7]:", language[7])
+
+
+print("language[-13]  :", language[-13])
+print("len(language) :", len(language))
+print("language[len(language)-3]  :", language[len(language) - 3])
